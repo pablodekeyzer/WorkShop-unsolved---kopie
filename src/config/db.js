f0fb75ba5658ec1ab8e216ce96c7b826055a44e6
pablo.dekeyzer <pablo.dekeyzer@student.odisee.be>
2022-06-08 17:09:41 +0200

    remove url

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDb = () => {
-  return mongoose.connect('mongodb://pablo:sickpass123@cluster0-shard-00-00.5plpr.mongodb.net:27017,cluster0-shard-00-01.5plpr.mongodb.net:27017,cluster0-shard-00-02.5plpr.mongodb.net:27017/?ssl=true&replicaSet=atlas-7j4atl-shard-0&authSource=admin&retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true }, err => {
+  return mongoose.connect('', { useUnifiedTopology: true, useNewUrlParser: true }, err => {
     if (err) {
       console.log("Connection to Database failed.");
     }
